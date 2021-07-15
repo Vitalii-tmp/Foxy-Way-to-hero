36ace7e78ee9406f82118f250371bfe90cf41369
Vitalii-tmp <vitaliilchuk6@gmail.com>
2021-07-15 23:33:02 +0300

    transitions

--- VisualStudioProject/Foxy_Way_to_hero/Foxy_Way_to_hero/RestartButton.cpp
+++ VisualStudioProject/Foxy_Way_to_hero/Foxy_Way_to_hero/RestartButton.cpp
@@ -5,6 +5,8 @@ void godot::RestartButton::_register_methods()
 	register_method("_ready", &RestartButton::_ready);
 	register_method("_process", &RestartButton::_process);
 	register_method("_on_restart_button_pressed", &RestartButton::_on_restart_button_pressed);
+	register_method("_restart_world", &RestartButton::_restart_world);
+	
 }
 
 void godot::RestartButton::_init()
@@ -16,6 +18,9 @@ void godot::RestartButton::_ready()
 	connect("pressed", this, "_on_restart_button_pressed");
 
 	_resource_loader = ResourceLoader::get_singleton();
+
+	_timer = Timer::_new();
+	this->add_child(_timer);
 }
 
 void godot::RestartButton::_process(float delta)
@@ -35,17 +40,40 @@ void godot::RestartButton::_process(float delta)
 
 void godot::RestartButton::_on_restart_button_pressed()
 {
+
+	_button_name = this->get_name();
+	Ref<PackedScene> fade_out = _resource_loader->load("res://Scenes/Effects/FadeOut.tscn");
+	this->add_child(fade_out->instance());
+
+	if (!_timer->is_connected("timeout", this, "_restart_world"))
+	{
+		_timer->connect("timeout", this, "_restart_world");
+		_timer->start(1);
+	}
+}
+
+void godot::RestartButton::_restart_world()
+{
 	Ref<PackedScene> world = _resource_loader->load("res://World.tscn");
 
-	if (get_name() == "RestartButton") {
+	if (_button_name == "RestartButton") {
 		get_node("/root/World")->set_name("to_delete");
 		get_node("/root/to_delete")->queue_free();
 		get_tree()->set_pause(false);
 	}
-	else if(get_name() == "RestartDeathButton")
+	else if (_button_name == "RestartDeathButton")
 	{
 		get_node("/root/DeathMenu")->set_name("to_delete");
 		get_node("/root/to_delete")->queue_free();
 	}
 	get_node("/root")->add_child(world->instance());
+
+
+
+	if (_timer->is_connected("timeout", this, "_restart_world"))
+	{
+		_timer->disconnect("timeout", this, "_restart_world");
+	}
+
+
 }
